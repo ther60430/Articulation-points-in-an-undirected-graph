c5008e3aff8cc1e732e1d9d7d719d22711c6bcf7
ther <1990592590@qq.com>
2025-12-11 18:47:31 +0800

    2

--- header.h
+++ header.h
@@ -1,3 +1,20 @@
 #pragma once
 #include<iostream>
 using namespace std;
+typedef struct EdgeNode
+{
+	int adjvex;
+	int weight;
+	struct EdgeNode* next;
+};
+typedef struct vertex
+{
+	int data;
+	EdgeNode* firstnode;
+};
+class adj_graph
+{
+public:
+	int vexnum, edgenum;
+	vertex* adjlist;
+};
